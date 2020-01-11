1ad7b35633965b3f5065694219265a5931902ea9
tomas orta <rodriguez.tomas@ymail.com>
2020-01-11 16:22:54 -0500

    Boundary canadian map by provinces territory

---
+++ Data/e.js
@@ -0,0 +1,117 @@
+var ProvData =   {
+ "type": "FeatureCollection",
+ "features": [
+   {
+     "type": "Feature",
+     "id": "01",
+     "properties": {
+       "Name": "Alberta",
+       "short": "AB",
+       "rank": 4,
+       "population": 4067175,
+       "capital": "Edmonton",
+       "Description": "Gimme espresso bars are found in New York City and Upstate New York."
+     },
+     "geometry": {
+       "type": "Point",
+       "coordinates": [-113.423,53.514]
+     }
+   },
+   {
+     "type": "Feature",
+     "id": "02",
+     "properties": {
+       "Name": "British Columbia",
+       "short": "BC",
+       "rank": 3,
+       "population": 4648055,
+       "capital": "Victoria",
+       "Description": "Specializes in spatial and environmental data analysis and statistics, geographic information systems and custom data design."
+     },
+     "geometry": {
+       "type": "Point",
+       "coordinates": [-127.134,54.521]
+     }
+   },
+   {
+     "type": "Feature",
+     "id": "03",
+     "properties": {
+       "Name": "Manitoba",
+       "short": "MB",
+       "rank": 5,
+       "population": 1278365,
+       "capital": "Winnipeg",
+       "Description": "Inn and cocktail bar in a beautifully restored historic mansion."
+     },
+     "geometry": {
+       "type": "Point",
+       "coordinates": [-96.899,54.927]
+     }
+   },
+   {
+     "type": "Feature",
+     "id": "04",
+     "properties": {
+       "Name": "New Brunswick",
+       "short": "NB",
+       "rank": 8,
+       "population": 747101,
+       "capital": "Fredericton",
+       "Description": "Inn and cocktail bar in a beautifully restored historic mansion."
+     },
+     "geometry": {
+       "type": "Point",
+       "coordinates": [-66.753,46.514]
+     }
+   },
+   {
+     "type": "Feature",
+     "id": "05",
+     "properties": {
+       "Name": "Newfoundland And Labrador",
+       "short": "NL",
+       "rank": 8,
+       "population": 519716,
+       "capital": "Fredericton",
+       "Description": "Inn and cocktail bar in a beautifully restored historic mansion."
+     },
+     "geometry": {
+       "type": "Point",
+       "coordinates": [ -61.084,53.723]
+     }
+   },
+   {
+     "type": "Feature",
+     "id": "06",
+     "properties": {
+       "Name": "Nova Scotia",
+       "short": "NS",
+       "rank": 6,
+       "population":  923598,
+       "capital": "Halifax",
+       "Description": "Inn and cocktail bar in a beautifully restored historic mansion."
+     },
+     "geometry": {
+       "type": "Point",
+       "coordinates": [ -63.149,53.723]
+     }
+   },
+   {
+     "type": "Feature",
+     "id": "07",
+     "properties": {
+       "Name": "Yukon",
+       "short": "YT",
+       "rank": 13,
+       "population":  35874,
+       "capital": "Whitehorse",
+       "Description": "Inn and cocktail bar in a beautifully restored historic mansion."
+     },
+     "geometry": {
+       "type": "Point",
+       "coordinates": [-137.109,64.718]
+     }
+   }
+ ]
+}
